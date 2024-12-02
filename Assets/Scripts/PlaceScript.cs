f354676073e8ca362f6b9088ba5fc32869f4808e
Ploshiey <maxkelly04@gmail.com>
2024-12-02 14:57:00 +1100

    Track placement fully functional

--- Assets/Scripts/PlaceScript.cs
+++ Assets/Scripts/PlaceScript.cs
@@ -34,60 +34,66 @@ public class PlaceScript : MonoBehaviour
         mouseY = mousePos.y;
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
+            if (mouseY > 2.5f)
             {
-                if (mouseY > 2.5f)
+                if (track[trackCount].transform.tag == "Up" && track[trackCount].transform.position.y < yBounds)
                 {
-                    if (track[trackCount].transform.tag == "Up")
-                    {
-                        trackCount++;
-                        track[trackCount] = Instantiate(upPiece, new Vector3(track[trackCount - 1].transform.position.x, track[trackCount - 1].transform.position.y + 1, mousePos.z), Quaternion.Euler(270f, 0, 0));
-                    }
-                    else if (track[trackCount].transform.tag == "Straight")
-                    {
-                        trackCount++;
-                        track[trackCount] = Instantiate(leftPiece, new Vector3(track[trackCount - 1].transform.position.x + 1, track[trackCount - 1].transform.position.y, mousePos.z), Quaternion.Euler(270f, 0, 0));
-                        track[trackCount].transform.tag = "Up";
-                    }
+                    trackCount++;
+                    track[trackCount] = Instantiate(upPiece, new Vector3(track[trackCount - 1].transform.position.x, track[trackCount - 1].transform.position.y + 1, mousePos.z), Quaternion.Euler(270f, 0, 0));
+                    track[trackCount].transform.SetParent(track[0].transform);
                 }
-                else if (mouseY < -2.5f)
+                else if (track[trackCount].transform.tag == "Straight" && track[trackCount].transform.position.y < yBounds)
                 {
-                    if (track[trackCount].transform.tag == "Straight")
-                    {
-                        trackCount++;
-                        track[trackCount] = Instantiate(rightPiece, new Vector3(track[trackCount - 1].transform.position.x + 1, track[trackCount - 1].transform.position.y, mousePos.z), Quaternion.Euler(270f, 0, 0));
-                        track[trackCount].transform.tag = "Down";
-                    }
-                    else if (track[trackCount].transform.tag == "Down")
-                    {
-                        trackCount++;
-                        track[trackCount] = Instantiate(downPiece, new Vector3(track[trackCount - 1].transform.position.x, track[trackCount - 1].transform.position.y - 1, mousePos.z), Quaternion.Euler(270f, 0, 0));
-                    }
+                    trackCount++;
+                    track[trackCount] = Instantiate(leftPiece, new Vector3(track[trackCount - 1].transform.position.x + 1, track[trackCount - 1].transform.position.y, mousePos.z), Quaternion.Euler(270f, 0, 0));
+                    track[trackCount].transform.tag = "Up";
+                    track[trackCount].transform.SetParent(track[0].transform);
                 }
-                else if (mouseX > 0f)
+            }
+            else if (mouseY < -2.5f)
+            {
+                if (track[trackCount].transform.tag == "Straight" && track[trackCount].transform.position.y > -yBounds)
                 {
-                    if (track[trackCount].transform.tag == "Up")
-                    {
-                        trackCount++;
-                        track[trackCount] = Instantiate(rightPiece, new Vector3(track[trackCount - 1].transform.position.x, track[trackCount - 1].transform.position.y + 1, mousePos.z), Quaternion.Euler(270f, 0, 0));
-                        track[trackCount].transform.tag = "Straight";
-                    }
-                    else if (track[trackCount].transform.tag == "Down")
-                    {
-                        trackCount++;
-                        track[trackCount] = Instantiate(leftPiece, new Vector3(track[trackCount - 1].transform.position.x, track[trackCount - 1].transform.position.y - 1, mousePos.z), Quaternion.Euler(270f, 0, 0));
-                        track[trackCount].transform.tag = "Straight";
-                    }
-                    else
-                    {
-                        trackCount++;
-                        track[trackCount] = Instantiate(straightPiece, new Vector3(track[trackCount - 1].transform.position.x + 1, track[trackCount - 1].transform.position.y, mousePos.z), Quaternion.Euler(270f, 0, 0));
-                    }
+                    trackCount++;
+                    track[trackCount] = Instantiate(rightPiece, new Vector3(track[trackCount - 1].transform.position.x + 1, track[trackCount - 1].transform.position.y, mousePos.z), Quaternion.Euler(270f, 0, 0));
+                    track[trackCount].transform.tag = "Down";
+                    track[trackCount].transform.SetParent(track[0].transform);
                 }
-                else if (mouseX <  0f)
+                else if (track[trackCount].transform.tag == "Down" && track[trackCount].transform.position.y > -yBounds)
                 {
+                    trackCount++;
+                    track[trackCount] = Instantiate(downPiece, new Vector3(track[trackCount - 1].transform.position.x, track[trackCount - 1].transform.position.y - 1, mousePos.z), Quaternion.Euler(270f, 0, 0));
+                    track[trackCount].transform.SetParent(track[0].transform);
+                }
+            }
+            else if (mouseX > 0f)
+            {
+                if (track[trackCount].transform.tag == "Up")
+                {
+                    trackCount++;
+                    track[trackCount] = Instantiate(rightPiece, new Vector3(track[trackCount - 1].transform.position.x, track[trackCount - 1].transform.position.y + 1, mousePos.z), Quaternion.Euler(0, -90, 90));
+                    track[trackCount].transform.tag = "Straight";
+                    track[trackCount].transform.SetParent(track[0].transform);
+                }
+                else if (track[trackCount].transform.tag == "Down")
+                {
+                    trackCount++;
+                    track[trackCount] = Instantiate(leftPiece, new Vector3(track[trackCount - 1].transform.position.x, track[trackCount - 1].transform.position.y - 1, mousePos.z), Quaternion.Euler(180f, -90, 90));
+                    track[trackCount].transform.tag = "Straight";
+                    track[trackCount].transform.SetParent(track[0].transform);
+                }
+                else
+                {
+                    trackCount++;
+                    track[trackCount] = Instantiate(straightPiece, new Vector3(track[trackCount - 1].transform.position.x + 1, track[trackCount - 1].transform.position.y, mousePos.z), Quaternion.Euler(270f, 0, 0));
+                    track[trackCount].transform.SetParent(track[0].transform);
+                }
+            }
+            else if (mouseX <  0f)
+            {
+                if (track[trackCount] != track[0])
+                {
+                    track[trackCount].transform.SetParent(null);
                     Destroy(track[trackCount]);
                     trackCount--;
                 }
